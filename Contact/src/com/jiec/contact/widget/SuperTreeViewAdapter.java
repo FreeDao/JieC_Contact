adf1fa9049e21bf5a0aa8e16579a66cce94bfc04
wcj <wo153@126.com>
2014-11-30 09:56:14 +0800

    整理

---
+++ Contact/src/com/jiec/contact/widget/SuperTreeViewAdapter.java
@@ -0,0 +1,100 @@
+
+package com.jiec.contact.widget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+
+public class SuperTreeViewAdapter extends BaseExpandableListAdapter {
+
+    static public class SuperTreeNode {
+        Object parent;
+
+        // 二级树形菜单的结构体
+        List<TreeViewAdapter.TreeNode> childs = new ArrayList<TreeViewAdapter.TreeNode>();
+    }
+
+    private List<SuperTreeNode> superTreeNodes = new ArrayList<SuperTreeNode>();
+
+    private Context parentContext;
+
+    private OnChildClickListener stvClickEvent;// 外部回调函数
+
+    public SuperTreeViewAdapter(Context view, OnChildClickListener stvClickEvent) {
+        parentContext = view;
+        this.stvClickEvent = stvClickEvent;
+    }
+
+    public List<SuperTreeNode> GetTreeNode() {
+        return superTreeNodes;
+    }
+
+    public void UpdateTreeNode(List<SuperTreeNode> node) {
+        superTreeNodes = node;
+    }
+
+    public void RemoveAll() {
+        superTreeNodes.clear();
+    }
+
+    public Object getChild(int groupPosition, int childPosition) {
+        return superTreeNodes.get(groupPosition).childs.get(childPosition);
+    }
+
+    public int getChildrenCount(int groupPosition) {
+        return superTreeNodes.get(groupPosition).childs.size();
+    }
+
+    public ExpandableListView getExpandableListView() {
+        AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT, TreeViewAdapter.ItemHeight);
+        ExpandableListView superTreeView = new ExpandableListView(parentContext);
+        superTreeView.setLayoutParams(lp);
+        return superTreeView;
+    }
+
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    public Object getGroup(int groupPosition) {
+        return superTreeNodes.get(groupPosition).parent;
+    }
+
+    public int getGroupCount() {
+        return superTreeNodes.size();
+    }
+
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView,
+            ViewGroup parent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild,
+            View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
---
+++ Contact/src/com/jiec/utils/FTPClientUtils.java
@@ -0,0 +1,95 @@
+
+package com.jiec.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import android.content.Context;
+import android.os.Environment;
+
+public class FTPClientUtils {
+
+    private FTPClient ftp;
+
+    private static Context mContext;
+
+    private boolean connect(String path, String addr, int port, String username, String password)
+            throws Exception {
+        boolean result = false;
+        ftp = new FTPClient();
+        int reply;
+        ftp.connect(addr, port);
+        ftp.login(username, password);
+        ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+        reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            return result;
+        }
+        ftp.changeWorkingDirectory(path);
+        result = true;
+        return result;
+    }
+
+    private void upload(File file) throws Exception {
+        if (file.isDirectory()) {
+            ftp.makeDirectory(file.getName());
+            ftp.changeWorkingDirectory(file.getName());
+            String[] files = file.list();
+            for (int i = 0; i < files.length; i++) {
+                File file1 = new File(file.getPath() + "\\" + files[i]);
+                if (file1.isDirectory()) {
+                    upload(file1);
+                    ftp.changeToParentDirectory();
+                } else {
+                    File file2 = new File(file.getPath() + "\\" + files[i]);
+                    FileInputStream input = new FileInputStream(file2);
+                    ftp.storeFile(file2.getName(), input);
+                    input.close();
+                }
+            }
+        } else {
+            File file2 = new File(file.getPath());
+            FileInputStream input = new FileInputStream(file2);
+
+            ftp.storeFile(file2.getName(), input);
+            input.close();
+        }
+    }
+
+    public static void updateFile(Context context) {
+        mContext = context;
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                FTPClientUtils t = new FTPClientUtils();
+
+                try {
+                    t.connect("contact/", "114.215.153.4", 21, "pwftp", "woshiwbjso");
+                    File contactPath = new File(Environment.getExternalStorageDirectory(),
+                            "contact");
+                    if (contactPath.exists()) {
+                        File[] files = contactPath.listFiles();
+                        for (int i = 0; i < files.length; i++) {
+                            long time = System.currentTimeMillis();
+                            LogUtil.d("upload file :" + files[i].getAbsolutePath() + " start");
+                            t.upload(files[i]);
+                            LogUtil.d("upload file :" + files[i].getAbsolutePath() + " success"
+                                    + ", time = " + (System.currentTimeMillis() - time));
+                            files[i].delete();
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+    }
+}
