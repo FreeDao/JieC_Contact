656b720353a32ef0013281f93fe27c29f4726b26
WangMJ <wo153@126.com>
2014-12-05 16:51:30 +0800

    Revert "整理"
    This reverts commit adf1fa9049e21bf5a0aa8e16579a66cce94bfc04.

--- Contact/src/com/jiec/contact/widget/SuperTreeNode.java
+++
@@ -1,9 +0,0 @@
-package com.jiec.contact.widget;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SuperTreeNode {
-	public Object parent;
-	public List<TreeViewAdapter.TreeNode> childs = new ArrayList<TreeViewAdapter.TreeNode>();
-}
